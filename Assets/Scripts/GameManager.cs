f25787cb5e3bb9232f2ffd8f7011fbcca2fba738
Zihan Yang <zihan_yang_1996@outlook.com>
2025-04-27 14:27:01 -0400

    Add a GameManger (Singleton), but it doesn't do anything for now.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 单例
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                SetupInstance();
+            }
+            return _instance;
+        }
+    }
+
+
+    private void Awake()
+    {
+        // Make sure this is the only instance of GameManager
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject); // Don't destroy this object when loading a new scene
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject); // Destroy this object if another instance already exists
+        }
+    }
+
+    // Some logic to handle game state
+    public void ResetForNewLoop()
+    {
+
+    }
+    
+    private static void SetupInstance()
+    {
+        _instance = FindObjectOfType<GameManager>();
+        if (_instance == null)
+        {
+            GameObject gameObj = new GameObject("GameManager (Generated)");
+            _instance = gameObj.AddComponent<GameManager>();
+            DontDestroyOnLoad(gameObj);
+        }
+    }
+}
