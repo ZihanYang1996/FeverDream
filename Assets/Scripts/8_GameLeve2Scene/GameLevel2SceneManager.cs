671f71e9c50bdeecdcde95575a81eb6add3ef8f1
Zihan Yang <zihan_yang_1996@outlook.com>
2025-05-01 20:11:08 -0400

    Using existing material to build around the main game loop for testing. The main game loop is tested, there are several features need to be added before we can fill in the content.

---
+++ Assets/Scripts/8_GameLeve2Scene/GameLevel2SceneManager.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+
+public class GameLevel2SceneManager : MonoBehaviour
+{
+    public GameObject showPuzzleButton;
+    [SerializeField] private StageManager stageManager;
+
+    [SerializeField] private string NormalStageId = "Rocket";
+    [SerializeField] private string SecretStageId = "Horse";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (showPuzzleButton != null)
+        {
+            showPuzzleButton.SetActive(false); // hide initially
+        }
+
+        StartPuzzle();
+
+        if (stageManager != null)
+        {
+            stageManager.OnPuzzleComplete += HandlePuzzleResult;
+        }
+        else
+        {
+            Debug.LogError("StageManager reference not assigned in the inspector.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void GoToNextScene(string condition)
+    {
+        GameManager.Instance.GoToNextScene(condition);
+    }
+
+    public void StartPuzzle()
+    {
+        if (showPuzzleButton != null)
+        {
+            showPuzzleButton.SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("ShowPuzzleButton is not assigned in the inspector.");
+        }
+
+        StageManager stageManager = FindAnyObjectByType<StageManager>();
+        if (stageManager != null)
+        {
+            var mainStages = GameManager.Instance.GetMainStages();
+            stageManager.SetupStages(mainStages);
+        }
+        else
+        {
+            Debug.LogError("StageManager not found in the scene.");
+        }
+    }
+
+    private void HandlePuzzleResult(bool success, StageData stage)
+    {
+        if (success)
+        {
+            Debug.Log($"[Game Leve 1] Puzzle '{stage.id}' completed!");
+            GameManager.Instance.RegisterCompletedStage(stage);
+
+            // Determine the scene transition condition based on the stage ID
+            if (stage.id == NormalStageId)
+            {
+                GoToNextScene("NormalSolved");
+            }
+            else if (stage.id == SecretStageId)
+            {
+                GoToNextScene("SecretSolved");
+            }
+            else
+            {
+                // Maybe in the future we will handle wrong stages as well
+                GoToNextScene("WrongSolved");
+            }
+        }
+        else
+        {
+            Debug.Log("[Game Leve 1] Puzzle failed.");
+            GoToNextScene("TimeOut");
+        }
+    }
+}
