d4ea43826e83af2fe8318679a0a12f508edd8903
Zihan Yang <zihan_yang_1996@outlook.com>
2025-05-06 22:02:00 -0400

    Working on Game Leve 1 Scene. Commit for backup.

--- Assets/Scripts/5_GameLevel1Scene/GameLevel1SceneManager.cs
+++ Assets/Scripts/5_GameLevel1Scene/GameLevel1SceneManager.cs
@@ -1,25 +1,54 @@
+using System.Collections;
+using DialogueSystem;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GameLevel1SceneManager : MonoBehaviour
 {
-    public GameObject showPuzzleButton;
+    public GameObject startPuzzleButton;
     [SerializeField] private StageManager stageManager;
 
     [SerializeField] private string NormalStageId = "Boat";
-    [SerializeField] private string SecretStageId = "Camel";
+    [SerializeField] private string SecretStageId = "Cat";
+
+    [Header("Actors")]
+    [SerializeField] private GameObject sky;
+
+    [SerializeField] private GameObject sea;
+    [SerializeField] private GameObject largeBoat;
+    [SerializeField] private GameObject characterWithWood;
+    [SerializeField] private GameObject generatedTangramHolder;
+    [SerializeField] private GameObject blackScreenImage;
+
+    [Header("Dialogue")]
+    [SerializeField] private string dialogueFileName1;
+
+    [SerializeField] private string dialogueFileName2;
+    [SerializeField] private string dialogueFileName3;
+    [SerializeField] private string dialogueFileName4;
+
+    [SerializeField] private DialogueManager dialogueManager;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Set the initial state of the scene
+        sky.SetActive(true);
+        sea.SetActive(true);
+        largeBoat.SetActive(true);
+        characterWithWood.SetActive(false);
+
+
         // Increment the current level index
         GameManager.Instance.IncrementCurrentLevelIndex();
-        
-        if (showPuzzleButton != null)
+
+        if (startPuzzleButton != null)
         {
-            showPuzzleButton.SetActive(false); // hide initially
+            startPuzzleButton.SetActive(false); // hide initially
         }
 
-        StartPuzzle();
+        // StartPuzzle();
 
         if (stageManager != null)
         {
@@ -29,6 +58,9 @@ public class GameLevel1SceneManager : MonoBehaviour
         {
             Debug.LogError("StageManager reference not assigned in the inspector.");
         }
+
+        // Start the pre-puzzle animation
+        StartCoroutine(PlayPrePuzzleAnimationCoroutine());
     }
 
     // Update is called once per frame
@@ -43,9 +75,10 @@ public class GameLevel1SceneManager : MonoBehaviour
 
     public void StartPuzzle()
     {
-        if (showPuzzleButton != null)
+        if (startPuzzleButton != null)
         {
-            showPuzzleButton.SetActive(true);
+            startPuzzleButton.SetActive(true);
+            startPuzzleButton.GetComponent<ButtonEntranceEffect>().PlayEntranceAnimation();
         }
         else
         {
@@ -76,18 +109,32 @@ public class GameLevel1SceneManager : MonoBehaviour
             {
                 // Increment the completed level count
                 GameManager.Instance.IncrementCompletedLevel();
-                GoToNextScene("NormalSolved");
+                // Play the post-puzzle animation
+                PlayPostPuzzleAnimation(stage.id, () =>
+                {
+                    // After the animation is finished, go to the next scene
+                    GoToNextScene("NormalSolved");
+                });
             }
             else if (stage.id == SecretStageId)
             {
                 // Increment the completed level count
                 GameManager.Instance.IncrementCompletedLevel();
-                GoToNextScene("SecretSolved");
+                // Play the post-puzzle animation
+                PlayPostPuzzleAnimation(stage.id, () =>
+                {
+                    // After the animation is finished, go to the next scene
+                    GoToNextScene("SecretSolved");
+                });
             }
             else
             {
-                // Maybe in the future we will handle wrong stages as well
-                GoToNextScene("WrongSolved");
+                // Play the post-puzzle animation
+                PlayPostPuzzleAnimation(stage.id, () =>
+                {
+                    // After the animation is finished, go to the next scene
+                    GoToNextScene("WrongSolved");
+                });
             }
         }
         else
@@ -95,5 +142,170 @@ public class GameLevel1SceneManager : MonoBehaviour
             Debug.Log("[Game Leve 1] Puzzle failed.");
             GoToNextScene("TimeOut");
         }
+    }
+
+    private IEnumerator PlayPrePuzzleAnimationCoroutine()
+    {
+        // Delay before starting the animation
+        yield return new WaitForSeconds(2.0f);
+
+        // Play Dialogue 1
+        bool isDialogueFinished = false;
+        var dialogueAsset1 = DialogueLoader.LoadFromResources("Dialogue/" + dialogueFileName1);
+        if (dialogueAsset1 == null)
+        {
+            Debug.LogError($"Failed to load dialogue: {dialogueFileName1}");
+            yield break;
+        }
+
+        dialogueManager.PlayDialogue(dialogueAsset1, Language.ZH, () => { isDialogueFinished = true; });
+        yield return new WaitUntil(() => isDialogueFinished); // Wait until the dialogue is finished
+
+        // Wait a short time before starting the next dialogue
+        yield return new WaitForSeconds(1.0f);
+
+        // Play Dialogue 2
+        isDialogueFinished = false;
+        var dialogueAsset2 = DialogueLoader.LoadFromResources("Dialogue/" + dialogueFileName2);
+        if (dialogueAsset2 == null)
+        {
+            Debug.LogError($"Failed to load dialogue: {dialogueFileName2}");
+            yield break;
+        }
+
+        dialogueManager.PlayDialogue(dialogueAsset2, Language.ZH, () => { isDialogueFinished = true; });
+        yield return new WaitUntil(() => isDialogueFinished); // Wait until the dialogue is finished
+
+        // Wait a short time before starting the animation
+        yield return new WaitForSeconds(1.0f);
+
+        // Move the boat to the upper right corner (using ActorController)
+        Vector3 targetPosition = new Vector3(4.0f, 0.7f, 0.0f);
+        float moveAndScaleDuration = 3.0f;
+        bool isBoatMovementFinished = false;
+        var boatController = largeBoat.GetComponent<ActorController>();
+        if (boatController == null)
+        {
+            Debug.LogError("BoatController not found on the large boat.");
+            yield break;
+        }
+
+        boatController.MoveToPosition(targetPosition, moveAndScaleDuration, () => { isBoatMovementFinished = true; });
+
+        // Scale down the boat at the same time
+        Vector3 targetScale = new Vector3(0.25f, 0.25f, 1.0f);
+        bool isBoatScaleFinished = false;
+        boatController.ScaleTo(targetScale, moveAndScaleDuration, () => { isBoatScaleFinished = true; });
+
+        // Wait until 1/3 of the boat movement is finished
+        yield return new WaitForSeconds(moveAndScaleDuration / 3.0f);
+
+        // Set the character with wood to active
+        characterWithWood.SetActive(true);
+        // Get the actor controller of the character with wood
+        var characterController = characterWithWood.GetComponent<ActorController>();
+        // Set the alpha of the character with wood to 0 (it self and also the child)
+        characterController.SetAlphaInstantly(0f);
+        // Fade in the character with wood
+        float fadeDuration = 1.0f;
+        bool isCharacterFadeFinished = false;
+        characterController.FadeToAlpha(1.0f, fadeDuration, () => { isCharacterFadeFinished = true; });
+
+        // Wait until both movements are finished for the boat and the fade in is finished for the character
+        yield return new WaitUntil(() => isBoatMovementFinished && isBoatScaleFinished && isCharacterFadeFinished);
+
+        // Wait a short time before starting the next dialogue
+        yield return new WaitForSeconds(2.0f);
+
+        // Play Dialogue 3
+        isDialogueFinished = false;
+        var dialogueAsset3 = DialogueLoader.LoadFromResources("Dialogue/" + dialogueFileName3);
+        if (dialogueAsset3 == null)
+        {
+            Debug.LogError($"Failed to load dialogue: {dialogueFileName3}");
+            yield break;
+        }
+
+        dialogueManager.PlayDialogue(dialogueAsset3, Language.ZH, () => { isDialogueFinished = true; });
+        yield return new WaitUntil(() => isDialogueFinished);
+
+        // Sink the boat
+        // Set the alpha of the large boat to 0
+        // Move the large boat to the bottom of the screen
+        float fadeAndMoveDuration = 2.0f;
+        bool isBoatFadeFinished = false;
+        bool isBoatMoveFinished = false;
+        targetPosition = new Vector3(4.0f, -4.0f, 0.0f);
+        largeBoat.GetComponent<ActorController>().FadeToAlpha(0f, fadeAndMoveDuration, () => { isBoatFadeFinished = true; });
+        // Move the large boat to the bottom of the screen
+        largeBoat.GetComponent<ActorController>().MoveToPosition(targetPosition, fadeAndMoveDuration, () => { isBoatMoveFinished = true; });
+        // Wait until both fade and move are finished
+        yield return new WaitUntil(() => isBoatFadeFinished && isBoatMoveFinished);
+        // Disable the large boat
+        largeBoat.SetActive(false);
+        
+        // Wait a short time before starting the next dialogue
+        yield return new WaitForSeconds(1.0f);
+
+        // Play Dialogue 4
+        isDialogueFinished = false;
+        var dialogueAsset4 = DialogueLoader.LoadFromResources("Dialogue/" + dialogueFileName4);
+        if (dialogueAsset4 == null)
+        {
+            Debug.LogError($"Failed to load dialogue: {dialogueFileName4}");
+            yield break;
+        }
+
+        dialogueManager.PlayDialogue(dialogueAsset4, Language.ZH, () => { isDialogueFinished = true; });
+        yield return new WaitUntil(() => isDialogueFinished);
+
+        // Display the puzzle button (start puzzle)
+        StartPuzzle();
+    }
+
+    private void PlayPostPuzzleAnimation(string stageID, System.Action onComplete)
+    {
+        // Play the post-puzzle animation based on the stage ID
+        if (stageID == "Boat")
+        {
+            // Play the normal stage post-puzzle animation
+            Debug.Log($"Playing post-puzzle animation for Normal Stage: {stageID}");
+            // Add your animation logic here
+            StartCoroutine(PlayBoatAnimationCoroutine((() => { onComplete?.Invoke(); })));
+        }
+        else if (stageID == "Cat")
+        {
+            // Play the secret stage post-puzzle animation
+            Debug.Log($"Playing post-puzzle animation for Secret Stage: {stageID}");
+            // Add your animation logic here
+        }
+        else
+        {
+            Debug.LogWarning($"No specific post-puzzle animation for stage: {stageID}");
+        }
+    }
+
+    private IEnumerator PlayBoatAnimationCoroutine(System.Action onFinish)
+    {
+        // Wait for a short time before starting the animation
+        yield return new WaitForSeconds(1.0f);
+
+        // Fade in the black screen
+        bool isFadeOutComplete = false;
+        if (blackScreenImage != null)
+        {
+            blackScreenImage.SetActive(true);
+            blackScreenImage.GetComponent<BlackScreenController>()?.StartFadeOut((() => { isFadeOutComplete = true; }));
+        }
+        // Wait for the fade-in to complete
+        yield return new WaitUntil(() => isFadeOutComplete);
+        Debug.Log("Fade out complete");
+        blackScreenImage.SetActive(false);
+        
+        
+
+        // Call the onComplete action after the animation is finished
+        // onFinish?.Invoke();
+        // yield return new WaitForSeconds(1.0f);
     }
 }
